06545609e4a95fcd67d39ac5ef343fe258148b95
Kevin Robles <kevinronu@gmail.com>
2023-05-23 21:27:27 -0500

    add objConcat method

--- assignments.js
+++ assignments.js
@@ -307,3 +307,16 @@ function deepCount(input) {
   return count;
 }
 console.log(deepCount([1, 2, [3, 4, [5]]]));
+
+// Merged Objects.
+function objConcat(array) {
+  // let object = {};
+  // for (const element of array) Object.assign(object, element);
+  // return object;
+  return Object.assign({}, ...array);
+}
+let a = { 1: "1", 2: "2", 3: "3" },
+  b = { 3: "4", 5: "6", 6: "7", 7: "8" },
+  c = { 5: "9", 8: "9", 6: "12", 23: "35" },
+  o = [a, b, c];
+console.log(objConcat(o));
